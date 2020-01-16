a21b786c311ff394a4db87d098bcb677756c4a04
werley <werley_desouzasilva@hotmail.com>
2020-01-16 16:07:50 -0300

    Finalizando o ajuste das views

---
+++ src/AppMvcCompleta/Extensions/RazorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace AppMvcCompleta.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1
+                ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00")
+                : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
